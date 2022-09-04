5c9567840227c77e5a25614d857e4f39f558eb33
Shahaban Mallick <mallickshaban2@gmail.com>
2022-09-04 14:29:40 +0530

    initial commit
    blog site commit

---
+++ public/js/firebase.js
@@ -0,0 +1,11 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyCrOMeA9hnFoxRJX-VvuT_jj1728MuUWZg",
+    authDomain: "blogging-website-8bed9.firebaseapp.com",
+    projectId: "blogging-website-8bed9",
+    storageBucket: "blogging-website-8bed9.appspot.com",
+    messagingSenderId: "185375327486",
+    appId: "1:185375327486:web:d1f88d300a93feac6691af"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  let db = firebase.firestore();
